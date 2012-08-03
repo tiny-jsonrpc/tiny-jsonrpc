e9a6b3c88f025ade0ef78270f8dce56e4140d95e
Lon Ingram <lon@waterfallmobile.com>
2012-08-03 01:08:47 -0500

    Calling provides with no name returns all methods

--- specs/provide.spec.js
+++ specs/provide.spec.js
@@ -1,176 +1,35 @@
 var StreamServer = require('../lib/tiny-jsonrpc').StreamServer;
 var MockStream = require('./util/mock-stream');
 
-describe('StreamServer.provide', function () {
-    it('registers functions as JSON-RPC methods if named', function () {
+describe('StreamServer.provides', function () {
+    it('returns true if the server provides the passed method', function () {
         var stream = new MockStream();
         var server = new StreamServer();
-        var called = {};
-
-        server.provide(function foo() { called.foo = true; });
-        server.listen(stream);
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'foo'
-        }));
-
-        expect(called.foo).toBe(true);
-
-        server.provide(
-            function fiz() { called.fiz = true; },
-            function frob() { called.frob = true; });
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 2,
-            method: 'fiz'
-        }));
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 3,
-            method: 'frob'
-        }));
-
-        expect(called.fiz).toBe(true);
-        expect(called.frob).toBe(true);
-    });
-
-    it('throws if passed an anonymous function', function () {
-        var server = new StreamServer();
-
-        expect(function () { server.provide(function () { }) }).toThrow();
-        expect(function () { server.provide(function foo() {}, function () { }) }).toThrow();
-    });
-
-    it('registers methods of objects as JSON-RPC methods', function () {
-        var stream = new MockStream();
-        var server = new StreamServer();
-        var called = {};
-
-        server.provide({ foo: function () { called.foo = true; } });
-        server.listen(stream);
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'foo'
-        }));
-
-        expect(called.foo).toBe(true);
-
-        server.provide({
-            fiz: function () { called.fiz = true; },
-            wiz: function () { called.wiz = true; }
-        }, {
-            frob: function () { called.frob = true; }
-        });
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 2,
-            method: 'fiz'
-        }));
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 3,
-            method: 'wiz'
-        }));
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 4,
-            method: 'frob'
-        }));
-
-        expect(called.fiz).toBe(true);
-        expect(called.wiz).toBe(true);
-        expect(called.frob).toBe(true);
-    });
-
-    it('allows functions and objects in the same call', function () {
-        var stream = new MockStream();
-        var server = new StreamServer();
-        var called = {};
-
-        server.provide({
-            foo: function () { called.foo = true; }
-        }, function fiz() { called.fiz = true; });
-
-        server.listen(stream);
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'foo'
-        }));
-
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 2,
-            method: 'fiz'
-        }));
-
-        expect(called.foo).toBe(true);
-        expect(called.fiz).toBe(true);
-    });
-
-    it('throws when passed a duplicate name', function () {
-        var server = new StreamServer();
-        function fn1() {};
-        function fn2() {};
-
-        expect(function () { server.provide(function foo() { }, function foo() { }) }).toThrow();
-        expect(function () { server.provide({ foo: fn1 }, { foo: fn2 }) }).toThrow();
-        expect(function () { server.provide({ foo: fn1 }, function foo() {}) }).toThrow();
 
         server.provide(function foo() {});
-        expect(function () { server.provide(function foo() { }) }).toThrow();
-        expect(function () { server.provide({ foo: fn1 }) }).toThrow();
+
+        expect(server.provides('foo')).toBe(true);
     });
 
-    it('registers no methods if any cause it to throw', function () {
+    it('returns false if the server does not provide the passed method', function () {
         var stream = new MockStream();
         var server = new StreamServer();
-        var called = {};
 
-        try {
-            server.provide(function foo() { called.foo = true; }, function () {});
-        } catch (e) {}
+        expect(server.provides('fiz')).toBe(false);
 
-        server.listen(stream);
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'foo'
-        }));
+        server.provide(function foo() {});
 
-        expect(called.foo).not.toBe(true);
+        expect(server.provides('frob')).toBe(false);
     });
 
-    it('marshals named arguments', function () {
+    it('returns the names of all methods the server provides if called without arguments', function () {
         var stream = new MockStream();
         var server = new StreamServer();
-        var called = false;
 
-        server.provide(function foo(bar, baz) {
-            expect(bar).toBe(void undefined);
-            expect(baz).toBe(23);
-            called = true;
-        });
+        server.provide(function foo() {},
+            function fiz() {},
+            function frob() {});
 
-        server.listen(stream);
-        stream.foobar = true;
-        stream.emit('data', JSON.stringify({
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'foo',
-            params: {
-                baz: 23,
-                biz: 42
-            }
-        }));
-
-        expect(called).toBe(true);
+        expect(server.provides()).toEqual(['foo', 'fiz', 'frob']);
     });
 });
