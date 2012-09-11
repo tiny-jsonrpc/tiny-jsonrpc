3ad8e7753e7904aec34ef57e1f5e6e944bbe8c64
Lon Ingram <lon@waterfallmobile.com>
2012-09-10 23:42:54 -0500

    Implement Client
    Create a client like so:
        var client = new Client({ server: serverInstance });
    
    The server config option is mandatory and must be an instance of Server (or an
    object that implements the same interface).
    
    Client instances provide two methods: request and notify. They can be called
    with a config object:
        client.request({
            method: 'echo',
            params: [1, 2, 3],
            callback: function (error, result) { ... }
        });
    or with an argument list, where the last argument may be a callback:
        client.request('echo', 1, 2, 3, function (error, result) { ... });
    
    Only the config object form supports named params.
    
    Callbacks will be passed two arguments: the error and the result. One of these
    will be null. Client.notify ignores any callback.

--- dist/tiny-jsonrpc.js
+++ dist/tiny-jsonrpc.js
@@ -79,6 +79,149 @@
         module.exports = factory(require(requireBase + '/util'));
     } else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
+        define('tiny-jsonrpc/client',['./util'], factory);
+    } else {
+        // Browser globals
+        root.tinyJsonRpc = root.tinyJsonRpc || {};
+        root.tinyJsonRpc.Client = factory(tinyJsonRpc.util);
+    }
+}(this, function (util) {
+    var defaultConfig = {};
+
+    function Client(options) {
+        this.options = options =
+            util.clone(util.defaults(options || {}, defaultConfig));
+        this._server = options.server;
+        this._nextId = 0;
+    }
+
+    Client.prototype._makeRequest = function () {
+        var method, params, callback, id;
+        var args = Array.prototype.slice.call(arguments);
+        var lastArg = args.length - 1;
+        var request = {
+            jsonrpc: '2.0'
+        };
+
+        if (util.isObject(args[0]) &&
+            !util.isUndefined(args[0]) && !util.isNull(args[0])
+        ) {
+            // called with a config object
+            method = args[0].method;
+            params = args[0].params;
+            request.callback = args[0].callback;
+        } else {
+            // called with a method name and optional params and callback
+            method = args[0];
+            params = args.slice(1);
+        }
+
+        if (!util.isString(method)) {
+            throw 'Method must be a string';
+        }
+
+        if (params === null ||
+            (!util.isArray(params) && !util.isObject(params))
+        ) {
+            throw 'Params must be an object or array';
+        }
+
+        request.method = method;
+        if (params) {
+            request.params = params;
+        }
+
+        return request;
+    };
+
+    Client.prototype.request = function () {
+        var request = this._makeRequest.apply(this, arguments);
+        var callback;
+        var response;
+
+        request.id = this._nextId++;
+
+        if (request.callback) {
+            callback = request.callback
+            delete request.callback;
+        } else if (util.isArray(request.params) &&
+            util.isFunction(request.params[request.params.length - 1])
+        ) {
+            callback = request.params.pop();
+        }
+
+        try {
+            request = JSON.stringify(request);
+        } catch (e) {
+            throw 'Could not serialize request to JSON';
+        }
+
+        response = JSON.parse(this._server.respond(request));
+
+        if (callback) {
+            callback(response.error || null, response.result || null);
+        }
+    };
+
+    Client.prototype.notify = function () {
+        var request = this._makeRequest.apply(this, arguments);
+
+        delete request.callback;
+
+        try {
+            request = JSON.stringify(request);
+        } catch (e) {
+            throw 'Could not serialize request to JSON';
+        }
+
+        this._server.respond(request);
+    };
+
+    function parseArgs(args) {
+        args = args.split(/,\s*/);
+        var result = {};
+
+        for (var i = 0; i < args.length; i++) {
+            result[args[i]] = i;
+        }
+
+        return result;
+    }
+
+    function keys(o) {
+        if (Object.prototype.keys) {
+            return o.keys();
+        }
+
+        var result = [];
+        for (var k in o) {
+            if (o.hasOwnProperty(k)) {
+                result.push(k);
+            }
+        }
+
+        return result;
+    }
+
+    return Client;
+}));
+
+;(function (root, factory) {
+    var requireBase = '.';
+
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+
+        // PhantomJS support
+        requireBase = typeof phantom !== 'undefined' && phantom.requireBase ?
+            phantom.requireBase + '/tiny-jsonrpc/lib/tiny-jsonrpc' :
+            requireBase;
+
+        module.exports = factory(require(requireBase + '/util'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
         define('tiny-jsonrpc/server',['./util'], factory);
     } else {
         // Browser globals
@@ -461,16 +604,19 @@
             requireBase;
 
         module.exports = factory(
+            require(requireBase + '/tiny-jsonrpc/client'),
             require(requireBase + '/tiny-jsonrpc/server'),
             require(requireBase + '/tiny-jsonrpc/stream-server'));
     } else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define('tiny-jsonrpc',[
+            './tiny-jsonrpc/client',
             './tiny-jsonrpc/server', './tiny-jsonrpc/stream-server'
         ], factory);
     }
-}(function (Server, StreamServer) {
+}(function (Client, Server, StreamServer) {
     return {
+        Client: Client,
         Server: Server,
         StreamServer: StreamServer
     };
---
+++ test/client.js
@@ -0,0 +1,15 @@
+var Client = require('../lib/tiny-jsonrpc').Client;
+var expect = require('expect.js');
+
+describe('Client instances', function () {
+    it('provide a request method', function () {
+        var client = new Client();
+        expect(client.request instanceof Function).to.be(true);
+    });
+
+    it('provide a notify method', function () {
+        var client = new Client();
+        expect(client.notify instanceof Function).to.be(true);
+    });
+});
+
