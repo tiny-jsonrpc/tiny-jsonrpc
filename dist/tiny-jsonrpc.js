f12560bedb7c6e218f9ce4ec14f019c1d3441c5a
Lon Ingram <lon@waterfallmobile.com>
2012-09-13 00:42:21 -0500

    Implement StreamClient
    Works like Client (mostly), but expects a read-write stream as its server config
    option. The notable difference from Client is that callbacks are async, rather
    than sync.

--- dist/tiny-jsonrpc.js
+++ dist/tiny-jsonrpc.js
@@ -91,6 +91,11 @@
     function Client(options) {
         this.options = options =
             util.clone(util.defaults(options || {}, defaultConfig));
+
+        if (!options.server) {
+            throw 'The server config option is required';
+        }
+
         this._server = options.server;
         this._nextId = 0;
     }
@@ -134,6 +139,16 @@
         return request;
     };
 
+    Client.prototype._send = function (request) {
+        try {
+            request = JSON.stringify(request);
+        } catch (e) {
+            throw 'Could not serialize request to JSON';
+        }
+
+        return JSON.parse(this._server.respond(request));
+    };
+
     Client.prototype.request = function () {
         var request = this._makeRequest.apply(this, arguments);
         var callback;
@@ -150,13 +165,7 @@
             callback = request.params.pop();
         }
 
-        try {
-            request = JSON.stringify(request);
-        } catch (e) {
-            throw 'Could not serialize request to JSON';
-        }
-
-        response = JSON.parse(this._server.respond(request));
+        response = this._send(request);
 
         if (callback) {
             callback(response.error || null, response.result || null);
@@ -168,13 +177,7 @@
 
         delete request.callback;
 
-        try {
-            request = JSON.stringify(request);
-        } catch (e) {
-            throw 'Could not serialize request to JSON';
-        }
-
-        this._server.respond(request);
+        this._send(request);
     };
 
     function parseArgs(args) {
@@ -204,6 +207,111 @@
     }
 
     return Client;
+}));
+
+;(function (root, factory) {
+    var requireBase = '.';
+
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+
+        // PhantomJS support
+        requireBase = typeof phantom !== 'undefined' && phantom.requireBase ?
+            phantom.requireBase + '/tiny-jsonrpc/lib/tiny-jsonrpc' :
+            requireBase;
+
+        module.exports = factory(require(requireBase + '/client'),
+            require(requireBase + '/util'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define('tiny-jsonrpc/stream-client',['./client', './util'], factory);
+    } else {
+        // Browser globals
+        root.tinyJsonRpc = root.tinyJsonRpc || {};
+        root.tinyJsonRpc.StreamClient =
+            factory(tinyJsonRpc.Client, tinyJsonRpc.util);
+    }
+}(this, function (Client, util) {
+    function StreamClient(options) {
+        options.server = {
+            stream: options.server,
+            buffer: [],
+            full: false
+        };
+        Client.apply(this, arguments);
+
+        this._callbacks = {};
+
+        this._server.stream.on('data', this._onData.bind(this));
+        this._server.stream.on('drain', this._onDrain.bind(this));
+    }
+
+    StreamClient.prototype = new Client({
+        server: true
+    });
+    StreamClient.prototype.constructor = StreamClient;
+
+    StreamClient.prototype._send = function (request) {
+        var success;
+
+        if (this._server.full) {
+            this._server.buffer.push(request);
+        } else {
+            try {
+                request = JSON.stringify(request);
+            } catch (e) {
+                throw 'Could not serialize request to JSON';
+            }
+
+            this._server.full = !this._server.stream.write(request);
+        }
+    };
+
+    StreamClient.prototype.request = function () {
+        var request = this._makeRequest.apply(this, arguments);
+        var callback;
+        var response;
+
+        request.id = this._nextId++;
+
+        if (request.callback) {
+            callback = request.callback
+            delete request.callback;
+        } else if (util.isArray(request.params) &&
+            util.isFunction(request.params[request.params.length - 1])
+        ) {
+            callback = request.params.pop();
+        }
+
+        this._send(request);
+
+        if (callback && util.isNumber(request.id)) {
+            this._callbacks[request.id] = callback;
+        }
+    };
+
+    StreamClient.prototype._onData = function (response) {
+        response = JSON.parse(response);
+
+        if (!util.isUndefined(response.id) && this._callbacks[response.id]) {
+            this._callbacks[response.id](response.error || null,
+                response.result || null);
+            delete this._callbacks[response.id];
+        }
+    };
+
+    StreamClient.prototype._onDrain = function (request) {
+        var buffer = this._server.buffer.slice().reverse();
+
+        this._server.full = false;
+        while (buffer.length > 0) {
+            this._send(buffer.pop());
+        }
+    };
+
+    return StreamClient;
 }));
 
 ;(function (root, factory) {
@@ -545,7 +653,7 @@
     StreamServer.prototype.constructor = StreamServer;
 
     StreamServer.prototype._write = function (stream, what) {
-        var success
+        var success;
 
         if (stream.full) {
             stream.buffer.push(what);
@@ -605,18 +713,20 @@
 
         module.exports = factory(
             require(requireBase + '/tiny-jsonrpc/client'),
+            require(requireBase + '/tiny-jsonrpc/stream-client'),
             require(requireBase + '/tiny-jsonrpc/server'),
             require(requireBase + '/tiny-jsonrpc/stream-server'));
     } else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define('tiny-jsonrpc',[
-            './tiny-jsonrpc/client',
+            './tiny-jsonrpc/client', './tiny-jsonrpc/stream-client',
             './tiny-jsonrpc/server', './tiny-jsonrpc/stream-server'
         ], factory);
     }
-}(function (Client, Server, StreamServer) {
+}(function (Client, StreamClient, Server, StreamServer) {
     return {
         Client: Client,
+        StreamClient: StreamClient,
         Server: Server,
         StreamServer: StreamServer
     };
